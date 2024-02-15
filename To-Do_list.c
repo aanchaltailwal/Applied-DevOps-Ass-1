bda3808b311aa6b5b1129dd717611510e2c4e1af
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-02-15 23:43:01 +0530

    Added new Feature

--- To-Do_list.c
+++ To-Do_list.c
@@ -40,12 +40,25 @@ void markComplete(int index) {
     }
 }
 
+void deleteTask(int index) {
+    if (index >= 0 && index < numTasks) {
+        for (int i = index; i < numTasks - 1; i++) {
+            strcpy(tasks[i].description, tasks[i + 1].description);
+            tasks[i].completed = tasks[i + 1].completed;
+        }
+        numTasks--;
+        printf("Task deleted successfully!\n");
+    } else {
+        printf("Invalid task number!\n");
+    }
+}
+
 int main() {
     char description[MAX_TASK_LENGTH];
     int choice, index;
 
     do {
-        printf("\n1. Add Task\n2. Display Tasks\n3. Mark Task as Complete\n4. Exit\n");
+        printf("\n1. Add Task\n2. Display Tasks\n3. Mark Task as Complete\n4. Delete Task\n5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -64,12 +77,17 @@ int main() {
                 markComplete(index - 1);
                 break;
             case 4:
+                printf("Enter task number to delete: ");
+                scanf("%d", &index);
+                deleteTask(index - 1);
+                break;
+            case 5:
                 printf("Exiting...\n");
                 break;
             default:
                 printf("Invalid choice! Please try again.\n");
         }
-    } while (choice != 4);
+    } while (choice != 5);
 
     return 0;
 }
